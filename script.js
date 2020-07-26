7f319aa56ebc7a2b17858abb62728d923f313286
Beta-23 <alfred.dominguez@gmail.com>
2020-07-25 23:11:48 -0700

    Buid proxy server to fix cors-proxy error

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Get Quotes from API and proxyUrl
 async function getQuote () {
-	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+	const proxyUrl = 'https://quiet-retreat-53485.herokuapp.com/';
 	const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 	try {
 		const res = await fetch(proxyUrl + apiUrl);
 		const data = await res.json();
 		console.log(data);
 	} catch (error) {
+		getQuote();
 		console.log('Whoops, no quotes available', error);
 	}
 }
