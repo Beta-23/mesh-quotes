829fb7c0533df6a04dc946a0490f7fea382510f7
Beta-23 <alfred.dominguez@gmail.com>
2020-07-25 22:51:18 -0700

    Integrate quotes api

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Testing Mesh-Quotes!');
+// Get Quotes from API and proxyUrl
+async function getQuote () {
+	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+	const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+	try {
+		const res = await fetch(proxyUrl + apiUrl);
+		const data = await res.json();
+		console.log(data);
+	} catch (error) {
+		console.log('Whoops, no quotes available', error);
+	}
+}
+
+// Get Quote onload
+
+getQuote();
