640cf33db5c3292263e0686fac1c0f7570e3b24b
Beta-23 <alfred.dominguez@gmail.com>
2020-07-26 22:09:44 -0700

    Integrate API quote rendering via the DOM

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-// Get Quotes from API and proxyUrl via Heroku server
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+// Get Quote From API
 async function getQuote () {
+	// We need to use a Proxy URL to make our API call in order to avoid a CORS error via Heroku server
 	const proxyUrl = 'https://quiet-retreat-53485.herokuapp.com/';
-	const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+	const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 	try {
-		const res = await fetch(proxyUrl + apiUrl);
-		const data = await res.json();
-		console.log(data);
+		const response = await fetch(proxyUrl + apiUrl);
+		const data = await response.json();
+		authorText.innerText = data.quoteAuthor;
+		quoteText.innerText = data.quoteText;
 	} catch (error) {
 		getQuote();
-		console.log('Whoops, no quotes available', error);
 	}
 }
 
