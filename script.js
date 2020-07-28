d060285167fff9f221fae6d9e1bfb8065ef01406
Beta-23 <alfred.dominguez@gmail.com>
2020-07-27 17:54:05 -0700

    Test unknown author data

--- script.js
+++ script.js
@@ -5,31 +5,33 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-// Loader function
+// Loading Spinner Shown
 function loading () {
 	loader.hidden = false;
 	quoteContainer.hidden = true;
 }
 
-// Hide Loader
+// Remove Loading Spinner
 function complete () {
 	if (!loader.hidden) {
 		quoteContainer.hidden = false;
 		loader.hidden = true;
 	}
 }
+
 // Get Quote From API
 async function getQuote () {
 	loading();
-	// Custom Proxy URL to make our API call in order to avoid a CORS error
+	// We need to use a Proxy URL to make our API call in order to avoid a CORS error
 	const proxyUrl = 'https://quiet-retreat-53485.herokuapp.com/';
 	const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 	try {
 		const response = await fetch(proxyUrl + apiUrl);
 		const data = await response.json();
-		// Checks if Author field is blank and replace it with 'Unknown'
+		// Check if Author field is blank and replace it with 'Unknown'
 		if (data.quoteAuthor === '') {
 			authorText.innerText = 'Unknown';
+			console.log(data);
 		}
 		else {
 			authorText.innerText = data.quoteAuthor;
@@ -42,7 +44,7 @@ async function getQuote () {
 			quoteText.classList.remove('long-quote');
 		}
 		quoteText.innerText = data.quoteText;
-		// Stop Loader and Show Quote
+		// Stop Loading, Show Quote
 		complete();
 	} catch (error) {
 		getQuote();
